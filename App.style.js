60abf88cf288a7bd74af67cd0e6e7fa70a66a6b2
Hugo Mouchon <hugomouchonpro@gmail.com>
2023-06-16 15:27:53 +0200

    Mise en place du layout afin de faciliter le futur développement

---
+++ App.style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+
+    app: {
+        backgroundColor: "#F9F9F9",
+        flex: 1,
+    },
+
+    header: {
+        flex: 1,
+        backgroundColor: "green",
+    },
+
+    body: {
+        flex: 5,
+        backgroundColor: "yellow",
+    },
+
+    footer: {
+        backgroundColor: "grey",
+        height: 70,
+    },
+
+});
